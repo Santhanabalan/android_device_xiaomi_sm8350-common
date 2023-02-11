0dd8d58a708a7ac84987010a63f1d3c6e028ab55
Mathieu Chartier <mathieuc@google.com>
2023-02-11 10:17:52 +0000

    sm8350-common: Speed profile services and wifi-service to reduce RAM and storage
    Signed-off-by: EndCredits <alicization.han@gmail.com>
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- common.mk
+++ common.mk
@@ -461,6 +461,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     libtextclassifier_annotator_en_model \
