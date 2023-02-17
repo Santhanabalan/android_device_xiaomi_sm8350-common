29ff5d36d97fccbe4fdbdc537c798a194e58f329
FlowerSea0208 <zzp060208@gmail.com>
2023-02-17 22:31:07 +0800

    sm8350-common: Update perf hal to 2.3
    Change-Id: I7be9ed5752a43185b4b734a126e43aab0743fd4c

--- common.mk
+++ common.mk
@@ -454,7 +454,7 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.3.vendor
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
