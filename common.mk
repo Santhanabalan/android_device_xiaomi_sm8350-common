7ebd48bf27e062a4e42e1629ad00b47fc1666b27
Santhanabalan <bala2102002@gmail.com>
2023-05-06 18:57:12 +0530

    sm8350-common: Pixelify
    Initial PE Bringup

--- common.mk
+++ common.mk
@@ -422,7 +422,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-xtended
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
