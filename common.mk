8a72cde106a1b25b4bfd172d8f611a3727da8d17
Santhanabalan <bala2102002@gmail.com>
2023-07-02 17:30:16 +0530

    sm8350-common: Initial ColtOS Bringup

--- common.mk
+++ common.mk
@@ -423,7 +423,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-colt
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
